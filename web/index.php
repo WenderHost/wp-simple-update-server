5a4d4410a3cd86315925d3b1b1f852270ddf24e0
Michael Wender <michael@michaelwender.com>
2025-01-22 08:56:15 -0500

    Adding meta data

--- web/index.php
+++ web/index.php
@@ -10,7 +10,21 @@ $package_info = getLatestPackage();
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Sellers.json Editor</title>
+
+  <!-- Essential Meta Tags -->
+  <title>Sellers.json Editor WordPress Plugin</title>
+  <meta name="description" content="The Sellers.json Editor for WordPress provides a backend interface for editing your site's sellers.json.">
+  <meta name="keywords" content="sellers.json,wordpress plugin">
+  <meta name="author" content="Wenmark Digital Solutions">
+  <meta name="robots" content="index, follow">
+  
+  <!-- Open Graph Meta Tags (for social media sharing) -->
+  <meta property="og:title" content="Sellers.json Editor WordPress Plugin">
+  <meta property="og:description" content="The Sellers.json Editor is the first and only plugin for editing your sellers.json directly inside the WordPress admin.">
+  <meta property="og:image" content="https://sellers-json.wenmarkdigital.com/assets/og-image_1880x984.png">
+  <meta property="og:url" content="https://sellers-json.wenmarkdigital.com">
+  <meta property="og:type" content="website">
+
   <link rel="stylesheet" href="assets/flexboxgrid.min.css" />
   <link rel="stylesheet" href="assets/styles.css" />
 </head>
@@ -22,7 +36,7 @@ $package_info = getLatestPackage();
     <div class="row">
       <div class="col-xs-2 hide-mobile"><img src="assets/icon-256x256.jpg" /></div>
       <div class="col-xs">
-        <h1 class="plugin-title">Sellers.json Editor</h1>
+        <h1 class="plugin-title">Sellers.json Editor for WordPress</h1>
         <span class="byline">By <a href="https://wenmarkdigital.com">Wenmark Digital Solutions</a></span>
       </div>
       <div class="col-md-3 end-md"><?php if( $allow_download ){ ?><a href="<?= $package_info['package']; ?>" class="button">Download</a><?php } ?></div>
